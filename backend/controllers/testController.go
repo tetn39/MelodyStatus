7830f776d956c04d818cca0cb51ef55fcaada78e
tetn39 <tetn39@gmail.com>
2025-03-24 02:53:38 +0900

    add: swaggerを追加。調整は後程

--- backend/controllers/testController.go
+++ backend/controllers/testController.go
@@ -4,6 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary liveness probe
+// @Schemes
+// @Description do ping
+// @Tags Hello World
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /hello [get]
 func GetHelloWorld(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Hello World",
--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/tetn39/MelodyStatus/controllers"
+	"github.com/tetn39/MelodyStatus/docs"
 	"github.com/tetn39/MelodyStatus/initializers"
 )
 
@@ -16,31 +19,41 @@ func init() {
 	initializers.SpotifyOAuth()
 }
 
+// @title MelodyStatusのAPI一覧
+// @version 1.0
+// @license.name tetn39
+// @description MelodyStatusのAPI一覧だよ
 func main() {
-	fmt.Println("Hello World")
-	r := gin.Default()
+	route := SetupRoutes()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	fmt.Println("起動しました")
+
+	v1 := route.Group("/api/v1")
+	{
+		// エンドポイントの設定
+		v1.GET("/hello", controllers.GetHelloWorld)                      // Hello Worldエンドポイント
+		v1.GET("/musics", controllers.GetAllMusics)                      // 音楽データの取得エンドポイント
+		v1.POST("/musics", controllers.PostMusic)                        // 音楽データの登録エンドポイント
+		v1.GET("/auth/spotify/login", controllers.GetSpotifyLogin)       // Spotifyログインエンドポイント
+		v1.GET("/auth/spotify/callback", controllers.GetSpotifyCallback) // Spotifyコールバックエンドポイント
+	}
+	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	route.Run(":8080")
+}
+
+// SetupRoutesはGinのルーティングをセットアップする関数
+func SetupRoutes() *gin.Engine {
+	route := gin.Default()
+
 	// CORS設定
-	r.Use(cors.New(cors.Config{
+	route.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:3000"},
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders: []string{"Content-Type", "Authorization"},
 	}))
 
-	// hello world
-	r.GET("/api/v1/hello", controllers.GetHelloWorld)
+	// ルートエンドポイントの設定
+	route.GET("/", controllers.GetHelloWorld)
 
-	// 音楽データの取得
-	r.GET("/api/v1/musics", controllers.GetAllMusics)
-
-	// 音楽データの登録
-	r.POST("/api/v1/musics", controllers.PostMusic)
-
-	// spotify login
-	r.GET("/auth/spotify/login", controllers.GetSpotifyLogin)
-
-	// spotify callback
-	r.GET("/auth/spotify/callback", controllers.GetSpotifyCallback)
-
-	r.Run()
-
+	return route
 }
